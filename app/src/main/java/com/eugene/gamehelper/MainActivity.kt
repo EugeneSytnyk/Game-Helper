b0c1c01088eaae3f6d72b50e5b966b8b3e705297
Eugene Sytnyk <e.sytnyk@autodoc.eu>
2024-09-07 02:12:20 +0300

    Improve MainActivity code

--- app/src/main/java/com/eugene/gamehelper/MainActivity.kt
+++ app/src/main/java/com/eugene/gamehelper/MainActivity.kt
@@ -4,31 +4,38 @@ import android.app.Activity
 import android.content.Intent
 import android.media.projection.MediaProjectionManager
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        private const val SCREEN_CAPTURE_REQUEST_CODE = 100
+
+    private val startForResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            onActivityResult(result.data)
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startScreenCapture()
+        findViewById<Button>(R.id.btnStart).setOnClickListener {
+            startScreenCapture()
+        }
     }
 
     private fun startScreenCapture() {
-        val mediaProjectionManager = getSystemService(MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+        val mediaProjectionManager =
+            getSystemService(MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
         val captureIntent = mediaProjectionManager.createScreenCaptureIntent()
-        startActivityForResult(captureIntent, SCREEN_CAPTURE_REQUEST_CODE)
+        startForResult.launch(captureIntent)
     }
 
-    @Deprecated("This method has been deprecated in favor of using the Activity Result API\n      which brings increased type safety via an {@link ActivityResultContract} and the prebuilt\n      contracts for common intents available in\n      {@link androidx.activity.result.contract.ActivityResultContracts}, provides hooks for\n      testing, and allow receiving results in separate, testable classes independent from your\n      activity. Use\n      {@link #registerForActivityResult(ActivityResultContract, ActivityResultCallback)}\n      with the appropriate {@link ActivityResultContract} and handling the result in the\n      {@link ActivityResultCallback#onActivityResult(Object) callback}.")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == SCREEN_CAPTURE_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+    private fun onActivityResult(data: Intent?) {
+        data?.let {
             val serviceIntent = Intent(this, ScreenCaptureService::class.java).apply {
                 putExtras(data)
             }
